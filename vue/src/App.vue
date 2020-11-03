92927e53c6e36b555211ff13c4018ebc101349dc
tsukanov_roman <roman.tsukanov@devexpress.com>
2020-11-03 16:34:07 +0300

    Add Vue source code

---
+++ vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <DxDropDownButton
+        text="Sandra Johnson"
+        icon="user"
+        :items="actions"
+        key-expr="id"
+        display-expr="text"
+        @item-click="logAction"
+        :split-button="true"
+        @button-click="logButtonClick"
+    />
+</template>
+
+<script>
+import DxDropDownButton from 'devextreme-vue/drop-down-button';
+
+const actions = [
+    { id: 1, text: "My profile", icon: "user" },
+    { id: 2, text: "Messages", icon: "email" },
+    { id: 3, text: "Contacts", icon: "group" },
+    { id: 4, text: "Log out", icon: "runner" }
+];
+
+export default {
+    components: {
+        DxDropDownButton
+    },
+    data() {
+        return {
+            actions
+        }
+    },
+    methods: {
+        logAction(e) {
+            console.log(e.itemData.text + " was clicked");
+        },
+        logButtonClick() {
+            console.log("Main button was clicked");
+        }
+    }
+}
+</script>
