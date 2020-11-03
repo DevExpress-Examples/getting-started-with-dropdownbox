a8351e71f0afa2be16b5c268b04a29a95fe49c28
tsukanov_roman <roman.tsukanov@devexpress.com>
2020-11-03 16:41:38 +0300

    Add React source code

---
+++ react/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+
+import DropDownButton from 'devextreme-react/drop-down-button';
+
+const actions = [
+    { id: 1, text: "My profile", icon: "user" },
+    { id: 2, text: "Messages", icon: "email" },
+    { id: 3, text: "Contacts", icon: "group" },
+    { id: 4, text: "Log out", icon: "runner" }
+];
+
+class App extends React.Component {
+    logAction(e) {
+        console.log(e.itemData.text + " was clicked");
+    }
+
+    logButtonClick() {
+        console.log("Main button was clicked");
+    }
+
+    render() {
+        return (
+            <DropDownButton
+                text="Sandra Johnson"
+                icon="user"
+                items={actions}
+                keyExpr="id"
+                displayExpr="text"
+                onItemClick={this.logAction}
+                splitButton={true}
+                onButtonClick={this.logButtonClick}
+            />
+        );
+    }
+}
+export default App;
