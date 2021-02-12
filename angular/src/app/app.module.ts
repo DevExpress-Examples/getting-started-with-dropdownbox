008ebb6b55bf4ab5cd65237b10adc0d352547754
LexDX <42230789+LexDX@users.noreply.github.com>
2021-02-12 19:10:59 +0300

    fix imports

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
-import { DxDropDownButtonModule } from 'devextreme-angular';
+import { DxDropDownButtonModule } from 'devextreme-angular/ui/drop-down-button';
 
 @NgModule({
     declarations: [
